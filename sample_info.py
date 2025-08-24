5d4aee09a4d2e0460ca07a534b742dacb08199d9
Safintc <safintc7899@gmail.com>
2025-08-24 18:01:00 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
-USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+USER_SESSION = 'sfn_Bot'
+API_ID = 21116415
+API_HASH = '8e23c9d97d71d525741e33f6b3584f45'
+BOT_TOKEN = '8336456330:AAH_OLodxSc4aTxCdC0cD_lzYy3RDq0gcPs'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -11,14 +11,15 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [1346360451]
+CHANNELS = [-1003073675420, 'auth channel']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'Telegram'
+DATABASE_URI = "mongodb+srv://safin:x0EwcqRWeyafnQIo@cluster0.1qpfh4t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_NAME = 'safin'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+
 
 
