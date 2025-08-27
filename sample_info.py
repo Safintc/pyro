2762b47160f5b4e658a91f161f5d36c8d147f82a
Safintc <safintc7899@gmail.com>
2025-08-27 11:19:26 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -3,8 +3,8 @@ SESSION = 'Media_search'
 USER_SESSION = 'sfn_Bot'
 API_ID = 21116415
 API_HASH = '8e23c9d97d71d525741e33f6b3584f45'
-BOT_TOKEN = '8336456330:AAH_OLodxSc4aTxCdC0cD_lzYy3RDq0gcPs'
-USERBOT_STRING_SESSION = ''
+BOT_TOKEN = '8257948836:AAEyS8XeEALcLzdpLRnAUvO6b4pdUIxF4SQ'
+USERBOT_STRING_SESSION = 'BQFCNf8AcDnhigMZt643WfY7AyghykoeTug7yZiUK6o6HPAbxBjN5CrB1lJy5OYs6nzhvYnJLtKqhI9JgorINnGnGX1KIjlQGugPLnVTOa-uZpfbe9_HVP1fgHJKfrmsafZCjjUdO9xYIlHNyFyM62KbNtTQ-b-Uis5bHWH289jcmiynpy-FngQubWuuHs4nM4UThcpcfKJoreT0bB05jecX3nzRvf2Dtd9KdyGNrRKSnDvaPejWJWAiZxuwb74cAaar9z6Z5jNQGftaVW4xFrNApKDSaWMtl7hG_J0ROLWJhsjB14quwnptiHyqDKW0fW83d63iZHmrZryNPBkxLdknTAyJWAAAAABQP9SDAA'
 
 # Bot settings
 CACHE_TIME = 300
@@ -20,6 +20,7 @@ AUTH_CHANNEL = None
 DATABASE_URI = "mongodb+srv://safin:x0EwcqRWeyafnQIo@cluster0.1qpfh4t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_NAME = 'safin'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+
 
 
 
